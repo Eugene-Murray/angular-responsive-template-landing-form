02e20eba3182dcaf9f124b6788be331b8184d7d4
Eugene Murray <eugene.murray@gmail.com>
2025-06-11 19:36:54 +0100

    add files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./components/header/header";
+import { MainContent } from "./components/main-content/main-content";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, MainContent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
